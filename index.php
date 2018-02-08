07fd8b6dd156c61fd5bb5315335956ed53d9db5c
Adrian Trujillo <atrujillo@csumb.edu>
2018-02-08 05:26:07 +0000

    First

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+<meta charset="utf-8">
+<title>Programming Languages</title>
+<style>
+</style>
+<link href="css/styles.css" rel="stylesheet" type="text/css" />
+</head>
+<header>
+        <h1> Programming Languages </h1>
+</header>
+
+<body>
+    <nav>
+        
+        <a href="index.php"><strong>Home</strong></a>
+        <a href="c++.html">C++</a>
+        <a href="java.html">Java</a>
+        <a href="python.html">Python</a>
+        
+        
+    </nav>
+    
+	<img src="https://upload.wikimedia.org/wikipedia/commons/0/0d/Prog-languages.png">
+	<br>
+	    
+	There are more than 500 Programming Languages. Java, Python, JavaScript and C++ are the most demanding 
+	<a href="http://www.codingdojo.com/blog/7-most-in-demand-programming-languages-of-2018/" alt="codingdojo.com">programming languages</a> for 2018. 
+	
+	<footer>
+        <hr>
+        CST336 Internet Programming. 2018&copy; Trujillo <br />
+        <strong>Disclaimer:</strong>The information in this webpage is fictitous. <br />
+        It is used for academic purposes only.
+        <p> <img src="https://www.prepsportswear.com/media/images/college_logos/300x300/1749151_mktg_logo.png" width="100" height="100"></p>
+            
+    </footer>
+	
+</body>
+
+
+</html>
